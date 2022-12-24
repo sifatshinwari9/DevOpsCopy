901fe248ea787f3ecf127693d374609d8b86293f
Chima Nwadike <chimanwadike94@gmail.com>
2022-12-24 00:02:18 +0000

    added usehttpsredirect to config

--- CovidDashboard/Program.cs
+++ CovidDashboard/Program.cs
@@ -30,11 +30,12 @@ namespace CovidDashboard
             }
 
 
-            //app.UseHttpsRedirection();
+            
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
             });
+            app.UseHttpsRedirection();
             app.UseStaticFiles();            
 
             app.UseRouting();
