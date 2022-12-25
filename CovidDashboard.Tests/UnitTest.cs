310c89528d02a582b55272c4c22d96b28a181998
Chima Nwadike <austintappy@gmail.com>
2022-12-25 17:16:32 +0000

    changed the chart title

---
+++ CovidDashboard.Tests/UnitTest.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace CovidDashboard.Tests
+{
+    public class UnitTest
+    {
+        IWebDriver _driver;// = new ChromeDriver();
+
+        [SetUp]
+        public void Setup()
+        {
+           _driver = new ChromeDriver();
+           _driver.Navigate().GoToUrl("http://ec2-18-134-157-3.eu-west-2.compute.amazonaws.com/");
+        }
+
+
+        [Test]
+        public void ChartWrapperDisplayedTest()
+        {          
+            IWebElement barChart = _driver.FindElement(By.ClassName("highcharts-figure"));
+            Assert.That(barChart.Displayed, Is.True);
+        }
+
+        [Test]
+        public void ChartChartDisplayedTest()
+        {                
+            IWebElement barChart = _driver.FindElement(By.Id("container"));
+            Assert.That(barChart.Displayed, Is.True);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {                 
+            _driver.Quit();
+        }
+    }
+}
