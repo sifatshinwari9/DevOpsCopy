044940d86c5049c0d928273ef1568991c692bacc
sidfarheen <farisid200@gmail.com>
2023-01-06 21:52:08 +0000

    Revert "removed data access"
    This reverts commit 677ad403ec39714fc4c7ba5b1806fb77534f4db1.

---
+++ DataAccess/IRepository/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.IRepository
+{
+    public interface IBaseRepository<T>
+    {
+        List<T> GetAll();
+        List<T> GetByQuery(Delegate @delegate);
+        void Add(T entity);
+    }
+}
